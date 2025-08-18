ed155c7d3f78a7c305de94414064e3eb7f5cce3d
Sean Murphy <sean@iamseanmurphy.com>
2025-08-18 17:18:47 +0000

    4.1.0

--- lazy-blocks/build/control-url.asset.php
+++ lazy-blocks/build/control-url.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-element', 'wp-hooks'), 'version' => '056ff00883e7a9d075d6');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-element', 'wp-hooks'), 'version' => '8064fdca89d85429c3ef');
